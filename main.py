a37e213b32e252493fa513dfe122dfdd670b9df9
Loren Anunciacion <loren.anunciacion@gmail.com>
2024-05-29 11:42:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-from flask import Flask, jsonify
-
 from bs4 import BeautifulSoup
 import requests
+import datetime
+
+current_datetime = datetime.datetime.now()
+
+formatted_datetime = current_datetime.strftime("%d-%m-%Y %H:%M")
 
 def get_currency(in_currency, out_currency):
   url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
   content = requests.get(url).text
   soup = BeautifulSoup(content, 'html.parser')
   rate = soup.find("span", class_="ccOutputRslt").get_text()
-  rate = float(rate[:-4])
-
+  
+  
   return rate
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-  return '<h1>Currency Rate API</h1> <p>Example URL: /api/v1/usd-eur</p>'
-
-@app.route('/api/v1/<in_cur>-<out_cur>')
-def api(in_cur, out_cur):
-  rate = get_currency(in_cur, out_cur)
-  result_dictionary = {'input_currency':in_cur, 'output_currency':out_cur, 'rate':rate}
-  return jsonify(result_dictionary)
-  
-app.run(host='0.0.0.0')
+print(formatted_datetime)
+print("USD conversion: ")
+print(str(get_currency('USD', 'JPY')))
+print(str(get_currency('USD', 'KRW')))
+print(str(get_currency('USD', 'PHP')))
